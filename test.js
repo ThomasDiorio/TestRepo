859f2696750ce5ac80e7319146a7f9a670d33316
Thomas Diorio <trd9077@gmail.com>
2021-01-07 16:39:41 -0600

    nwe

---
+++ test.js
@@ -0,0 +1,39 @@
+let name = "tom";
+const scores = [0, 22, 39, 100, 5, 7];
+
+// for(let i = 0; i < scores.length; i++){
+//     console.log('Try #' + (i + 1));
+
+//     if(scores[i] === 0){
+//         console.log('oops');
+//         continue;
+//     }
+
+//     if(scores[i] === 100){
+//         console.log('congrats, you got a 100!');
+//         break;
+//     }
+// }
+let grade = 'A';
+switch(grade){
+    case 'A':
+        console.log('Best grade');
+        break;
+    case 'B':
+        console.log('Ok grade');
+        break;
+    case 'C':
+        console.log('bad grade');
+        break;
+    case 'D':
+        console.log('worst grade');
+        break;
+    default:
+        console.log('no grade');
+}
+
+
+
+
+//console.log(name);
+
